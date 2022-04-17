4a8d3c19f1f80932e7add2ec7f1a160d3f85de30
Opfes <xopfesx@gmail.com>
2022-04-17 00:52:38 -0500

    completed project 2 server code

--- Project 2/Lindsay_webserver.py
+++ Project 2/Lindsay_webserver.py
@@ -1,38 +1,35 @@
-#import socket module
 from socket import *
-import sys # In order to terminate the program
+from sqlite3 import connect
+import sys
 serverSocket = socket(AF_INET, SOCK_STREAM)
-#Prepare a sever socket
-#Fill in start
-#Fill in end
+serverPort = 12000
+serverSocket.bind(('',serverPort))
+serverSocket.listen(1)
+print("Server is listening on", serverPort)
 
 while True:
-    #Establish the connection
     print('Ready to serve...')
-    connectionSocket, addr = #Fill in start #Fill in end
+    connectionSocket, addr = serverSocket.accept()
     try:
-        message = #Fill in start #Fill in end
+        message = connectionSocket.recv(1024).decode()
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = #Fill in start #Fill in end
-        #Send one HTTP header line into socket
-        #Fill in start
-        #Fill in end
-        #Send the content of the requested file to the client
+        outputdata = f.read()
+        f.close()
+        
+        response = 'HTTP/1.0 200 OK\n\n'
+        connectionSocket.send(response.encode())
+        
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
         
-        connectionSocket.send("\r\n".encode())
-        
+        connectionSocket.send("\r\n".encode())        
         connectionSocket.close()
     except IOError:
-        #Send response message for file not found
-        #Fill in start
-        #Fill in end
-        #Close client socket
-        #Fill in start
-        #Fill in end
+        response = 'HTTP/1.0 404 Not Found\n\nFile Not Found'
+        connectionSocket.sendall(response.encode())
+        connectionSocket.close()
         print("error")
 
 serverSocket.close()
-sys.exit()#Terminate the program after sending the corresponding data
+sys.exit()
